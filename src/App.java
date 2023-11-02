02ec1eabcaa731800c224e6debe3b61e9b83277f
raulmelo87 <fraulbmelo@gmail.com>
2023-11-02 17:00:46 -0300

    implementação da classe pessoa com métodos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Pessoa p1 = new Pessoa("João", 20);
+        System.out.println("Primeira pessoa cadastrada é " + p1);
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,8 +1,32 @@
 public class Pessoa {
     //atributos
-    private String name;
+    private String nome;  
     private int idade;
 
-    //métodos
-    
+    //métodos getters and setters
+    public String getName() {
+        return nome;
+    }
+    public void setName(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+    //método construtor
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return ("Nome " + nome + " , idade: " + idade);
+    }
 }
