8e4ad2f91df5fd7c2a8a1589d384641a5cfadc0b
raulmelo87 <fraulbmelo@gmail.com>
2023-11-04 15:38:38 -0300

    Cadastro da segunda pessoa

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Pessoa p1 = new Pessoa("João", 20);
-        System.out.println("Primeira pessoa cadastrada é " + p1);
+        System.out.println("Primeira pessoa cadastrada " + p1);
+        Pessoa p2 = new Pessoa("Maria", 25);
+        System.out.println("Segunda pessoa cadastrada: " + p2);
     }
 }
